33d33952c5ca8be5573524f419b5500588232162
AetherPhx <aetherphx@gmail.com>
2024-09-01 17:21:48 -0500

    Integrate script to the new interface

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
 const UNSPLASH_KEY = "dWY1oytwX7XTfnn1XAlVZGQOTcxykA9Oxi-Oe8bkqm4"; // Llave de Acceso para la API
 const UNSPLASH_URL = "https://api.unsplash.com/search/photos?page=1"; // URL para la petición a la API
 
+const prepareSearch = () => {
+	const searchInput = document.querySelector(".search-bar__input");
+
+	searchInput.addEventListener("keyup", (event) => {
+		if (event.key === "Enter") {
+			const searchValue = searchInput.value;
+			console.log("Searching for:", searchValue);
+			searchImagesOfCategory(searchValue).then((data) => {
+				clearGallery();
+				searchInput.value = "";
+				printNewImages(data.results);
+			});
+		}
+	});
+};
+
+// Limpiar la galería
+const clearGallery = () => {
+	const gallery = document.querySelector(".gallery");
+	gallery.innerHTML = "";
+};
+
 // Petición a la API
 const searchImagesOfCategory = async (cat) => {
 	const url = `${UNSPLASH_URL}&query=${cat}&client_id=${UNSPLASH_KEY}`;
@@ -28,22 +50,30 @@ const getImageData = (newImg) => {
 	return { imgUrl, slug }; // Retorna los valores de la nueva img
 };
 
-// Pintar las imágenes en el HTML
+// Pintar las nuevas imágenes en el HTML
 const printNewImages = (imagesList) => {
-	const board = document.querySelector(".board");
+	const gallery = document.querySelector(".gallery");
 
 	imagesList.forEach((newImg) => {
 		const { imgUrl, slug } = getImageData(newImg);
-		const img = document.createElement("img");
-		img.src = imgUrl;
-		img.alt = slug;
-		board.appendChild(img);
+		const newItem = document.createElement("div");
+		newItem.classList.add("gallery__item");
+		newItem.innerHTML = `
+		<img
+			class="gallery__image"
+			src="${imgUrl}"
+			alt="${slug}"
+		/>
+		<div class="gallery__overlay">
+			<button class="gallery__overlay-button">Guardar</button>
+		</div>
+		`;
+
+		gallery.appendChild(newItem);
 	});
 };
 
 (startApp = () => {
 	console.log("Starting App...");
-
-	// ! Uncomment when Interface is ready
-	// searchImagesOfCategory("Messi").then((data) => printNewImages(data.results));
+	prepareSearch();
 })();
